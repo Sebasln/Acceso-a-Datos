3b216b8e506dfe654f1861f65c14065a3573bdf8
arman <arman@192.168.1.122>
2025-10-23 17:25:52 +0200

    añadiendo archivos

---
+++ Practica Final JavaIO Ficheros de Acceso Directo Secuencial Sebastián Silva/src/practica/Main.java
@@ -0,0 +1,13 @@
+package practica;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//añadiendo archivos
+		Scanner sc = new Scanner(System.in);
+	}
+
+}
