f3fc248447546389f1143f5e9c55e669faa4cd82
DAM <DAM@LAB-35-PC18>
2025-10-29 12:48:05 +0100

    primeros cambios clase

---
+++ Practica Final JavaIO Ficheros de Acceso Directo Secuencial Sebastián Silva/src/practica/Empleado.java
@@ -0,0 +1,47 @@
+package practica;
+
+import java.io.Serializable;
+
+public class Empleado implements Serializable{
+	private int identificacion;
+	private String nombre, password, cargo;
+	
+	public Empleado(int identificacion, String nombre, String password, String cargo) {
+		super();
+		this.identificacion = identificacion;
+		this.nombre = nombre;
+		this.password = password;
+		this.cargo = cargo;
+	}
+	
+	public int getIdentificacion() {
+		return identificacion;
+	}
+	public void setIdentificacion(int identificacion) {
+		this.identificacion = identificacion;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	
+	@Override
+	public String toString() {
+		return "Empleado [identificacion=" + identificacion + ", nombre=" + nombre + ", password=" + password
+				+ ", cargo=" + cargo + "]";
+	}	
+}
