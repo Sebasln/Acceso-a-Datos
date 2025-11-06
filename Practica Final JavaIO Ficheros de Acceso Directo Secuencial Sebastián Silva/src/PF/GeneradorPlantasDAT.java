3fcb3c18ae36942b74ca9e86a9c958771cab8800
arman <arman@192.168.1.121>
2025-11-06 18:46:52 +0100

    generadorPlantas

---
+++ Practica Final JavaIO Ficheros de Acceso Directo Secuencial Sebastián Silva/src/PF/GeneradorPlantasDAT.java
@@ -0,0 +1,32 @@
+package PF;
+
+import java.io.RandomAccessFile;
+
+public class GeneradorPlantasDAT {
+
+	public static void main(String[] args) {
+
+		try (RandomAccessFile raf = new RandomAccessFile("resources/plantas.dat", "rw")) {
+
+			for (int i = 0; i < 20; i++) {
+
+				int codigo = i + 1; 
+
+				float precio = 5.0f + (i * 2.5f);
+
+				int stock = 50 + (i * 5); 
+
+				raf.writeInt(codigo);
+				raf.writeFloat(precio);
+				raf.writeInt(stock);
+
+				System.out.println("Escribiendo: " + codigo + ", " + precio + ", " + stock);
+			}
+
+			System.out.println("Fichero 'plantas.dat' creado con éxito con 20 registros.");
+
+		} catch (Exception e) {
+			System.err.println("Error al generar 'plantas.dat': " + e.getMessage());
+		}
+	}
+}
